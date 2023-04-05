8200daf879d10b94856a8599cf679a4a43a267a1
carolaine-viana <carolsantos14@hotmail.com>
2023-04-05 18:13:14 -0300

    new project using cypress and cucumber

---
+++ CypressWithCucumber/cypress/e2e/Contact_Us.feature
@@ -0,0 +1,11 @@
+Feature: WebDriverUniversity - Contact us page
+
+  Scenario: Valid Contact Us Form Submission
+    Given I navigate to the webdriveruniversity homepage
+    When I click on the contact us button
+    And I type a first name
+    And I Type a last name
+    And I enter an email address
+    And I type a comment
+    And I click on the submit button 
+    Then U should be presented with a successfull contact submission message
---
+++ CypressWithCucumber/cypress/support/step_definitions/Contact_Us.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+import {When, Then} from "@badeball/cypress-cucumber-preprocessor";
+// When('I type a first name', () => {
+//   //cy.get('input').should('contain', 'first name')
+// })
+
+When(`I type a first name`, () => {
+  cy.get('input[placeholder="First Name"]').type('Carol')
+});
+
+When(`I Type a last name`, () => {
+  cy.get('input[placeholder="Last Name"]').type('Viana')
+});
+
+When(`I enter an email address`, () => {
+  cy.get('input[placeholder="Email Address"]').type('carol@hotmail.com')
+});
+
+When(`I type a comment`, () => {
+  cy.get('textarea[placeholder="Comments"]').type('Ola ola ola')
+});
+
+When(`I click on the submit button`, () => {
+  cy.get('input[value="SUBMIT"]').click()
+});
+
+Then(`U should be presented with a successfull contact submission message`, () => {
+  cy.get('#contact_reply').children('h1').should('contain', 'Thank You for your Message!')
+});
