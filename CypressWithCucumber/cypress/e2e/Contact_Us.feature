657e8790ce1cb8a61b64acb61ec9d601f45ad158
carolaine-viana <carolsantos14@hotmail.com>
2023-04-05 18:49:48 -0300

    implement random input

--- CypressWithCucumber/cypress/e2e/Contact_Us.feature
+++ CypressWithCucumber/cypress/e2e/Contact_Us.feature
@@ -8,13 +8,23 @@ Feature: WebDriverUniversity - Contact us page
     And I enter an email address
     And I type a comment
     And I click on the submit button 
-    Then U should be presented with a successfull contact submission message
+    Then U should be presented with a successfull contact us submission message
 
-  Scenario: Invalid Contact us Fom Submission
+  Scenario: Invalid Contact Us Fom Submission
     Given I navigate to the webdriveruniversity homepage
     When I click on the contact us button
     And I type a first name
     And I Type a last name
     And I type a comment
     And I click on the submit button 
-    Then U should be presented with a unsuccessfull contact submission message
+    Then U should be presented with a unsuccessfull contact us submission message
+
+  Scenario: Valid Contact Us Fom Submission - Using Specific Data
+    Given I navigate to the webdriveruniversity homepage
+    When I click on the contact us button
+    And I type a specific first name "Sarah"
+    And I type a specific last name "Woods"
+    And I type a specific email address "sarah@hotmail.com"
+    And I type a specific word "hello 123" and number 6788 within the comment input field
+    And I click on the submit button 
+    Then U should be presented with a successfull contact us submission message
--- CypressWithCucumber/cypress/support/step_definitions/Contact_Us.js
+++ CypressWithCucumber/cypress/support/step_definitions/Contact_Us.js
@@ -21,11 +21,29 @@ When(`I click on the submit button`, () => {
   cy.get('input[value="SUBMIT"]').click()
 });
 
-Then(`U should be presented with a successfull contact submission message`, () => {
-  cy.get('#contact_reply').children('h1').should('contain', 'Thank You for your Message!')
+Then(`U should be presented with a successfull contact us submission message`, () => {
+  //cy.get('#contact_reply').children('h1').should('contain', 'Thank You for your Message!')
+  //
+  cy.get('h1').should('have.text', 'Thank You for your Message!')
 });
 
-Then(`U should be presented with a unsuccessfull contact submission message`, () => {
-  cy.get('body').contains('all fields are required')
-  cy.get('body').contains('Invalid email address')
+Then(`U should be presented with a unsuccessfull contact us submission message`, () => {
+  //cy.get('body').contains('all fields are required')
+  cy.get('body').contains('Error: Invalid email address')
+});
+
+When(`I type a specific first name {string}`, (firstName) => {
+  cy.get('input[placeholder="First Name"]').type(firstName)
+});
+
+When(`I type a specific last name {string}`, (lastName) => {
+  cy.get('input[placeholder="Last Name"]').type(lastName)
+});
+
+When(`I type a specific email address {string}`, (email) => {
+  cy.get('input[placeholder="Email Address"]').type(email)
+});
+
+When(`I type a specific word {string} and number {int} within the comment input field`, (word, number) => {
+  cy.get('textarea[placeholder="Comments"]').type(word, number)
 });
